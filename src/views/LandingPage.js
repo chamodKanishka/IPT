1afa231dead054aa234a3cf2cf3548e0d43fcdeb
ChamodKanishka <chamodkanishka77@gmail.com>
2020-04-02 22:15:18 +0530

    add image to landig page and add some content to it

---
+++ src/views/LandingPage.js
@@ -0,0 +1,54 @@
+import React from "react";
+// nodejs library that concatenates classes
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+
+// core components
+import GridContainer from "../components/Grid/GridContainer.js";
+import GridItem from "../components/Grid/GridItem.js";
+import Button from "../components/CustomButtons/Button.js";
+import Parallax from "../components/Parallax/Parallax.js";
+
+import styles from "../assets/jss/material-kit-react/views/landingPage.js";
+// Sections for this page
+
+
+const useStyles = makeStyles(styles);
+
+export default function LandingPage(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Parallax filter image={require("../assets/img/landing-bg.jpg")}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem xs={14} sm={12} md={6}>
+              <h2 className={classes.title}>Starts Your Career With Us.</h2>
+              <h4>
+                Every landing page needs a small description after the big bold
+                title, that{"'"}s why we added this text here. Add here all the
+                information thats can make you or your product create the first
+                impression.
+              </h4>
+              <br />
+              <Button
+                color="danger"
+                size="lg"
+                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fas fa-play" />
+                Let's Start
+              </Button>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+    </div>
+  );
+}
