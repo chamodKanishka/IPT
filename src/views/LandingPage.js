49309e53fb2ee0475fa8e8381b9d0d06b237af78
ChamodKanishka <chamodkanishka77@gmail.com>
2020-04-02 22:29:50 +0530

    add header for landing page

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -7,6 +7,7 @@ import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
 
 // core components
+import Header from "../components/Header/Header.js";
 import GridContainer from "../components/Grid/GridContainer.js";
 import GridItem from "../components/Grid/GridItem.js";
 import Button from "../components/CustomButtons/Button.js";
@@ -23,6 +24,16 @@ export default function LandingPage(props) {
   const { ...rest } = props;
   return (
     <div>
+      <Header
+        color="transparent"
+        brand="Industrial Preparation Programme"
+        fixed
+        changeColorOnScroll={{
+          height: 400,
+          color: "white"
+        }}
+        {...rest}
+      />
       <Parallax filter image={require("../assets/img/landing-bg.jpg")}>
         <div className={classes.container}>
           <GridContainer>
