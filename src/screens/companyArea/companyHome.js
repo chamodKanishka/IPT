ef8f53b46b19a26034c1fd04011bbad723d364cf
ChamodKanishka <chamodkanishka77@gmail.com>
2020-04-12 17:04:20 +0530

    jobs page content and components are added to page

--- src/screens/companyArea/companyHome.js
+++ src/screens/companyArea/companyHome.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,21 +7,24 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import StudentHomeNav from './studentHomeNav';
-import Job from '../../images/jobs.jpg'
-
-
-
+import CompanyHomeNav from './companyHomeNav'
+import image from '../../images/person.jpg'
+import Job from '../../images/jobs.jpg';
+import IconButton from '@material-ui/core/IconButton';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
   
 
-export default  function StudentHome() {
+export default  function CompanyHome() {
     const classes = useStyles();
+    const theme = useTheme();
 
   return (
       <div>
-            <StudentHomeNav/>
+            <CompanyHomeNav/>
             <div className={classes.body}>
-                <h3 className={classes.activity}>Recently Viewed Jobs</h3>
+                <h3 className={classes.activity}>Recently Posted Jobs</h3>
                 <div className={classes.recent}>
             <Card className={classes.cardroot}>
             <CardActionArea>
@@ -42,10 +45,7 @@ export default  function StudentHome() {
             </CardActionArea>
             <CardActions>
                 <Button size="small" color="primary">
-                Apply
-                </Button>
-                <Button size="small" color="primary">
-                Requirements
+                Status
                 </Button>
             </CardActions>
             </Card>
@@ -68,11 +68,8 @@ export default  function StudentHome() {
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary">
-                Apply
-                </Button>
-                <Button size="small" color="primary">
-                Requirements
+            <Button size="small" color="primary">
+                Status
                 </Button>
             </CardActions>
             </Card>
@@ -95,11 +92,8 @@ export default  function StudentHome() {
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary">
-                Apply
-                </Button>
-                <Button size="small" color="primary">
-                Requirements
+            <Button size="small" color="primary">
+                Status
                 </Button>
             </CardActions>
             </Card>
@@ -122,11 +116,8 @@ export default  function StudentHome() {
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary">
-                Apply
-                </Button>
-                <Button size="small" color="primary">
-                Requirements
+            <Button size="small" color="primary">
+                Status
                 </Button>
             </CardActions>
             </Card>
@@ -134,103 +125,59 @@ export default  function StudentHome() {
             </div>
 
             <div className={classes.body}>
-                <h3 className={classes.activity}>Recently Viewed Jobs</h3>
-                <div className={classes.recent}>
-            <Card className={classes.cardroot}>
-            <CardActionArea>
+                <h3 className={classes.activity}>Recently Applicants</h3>
+                <div>
+                <Card className={classes.miniroot} elevation={3}>
+                <div className={classes.details}>
+                    <CardContent className={classes.content} >
+                    <Typography component="h5" variant="h5">
+                        Name Here
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                        job title
+                    </Typography>
+                    </CardContent>
+                </div>
                 <CardMedia
-                className={classes.media}
-                image={Job}
-                title="Contemplative Reptile"
+                    className={classes.cover}
+                    image={image}
+                    title="Live from space album cover"
                 />
-                <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    Title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    Short Description are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                    across all continents except Antarctica
-                </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-            <Button size="small" color="primary">
-                Status
-                </Button>
-            </CardActions>
-            </Card>
-
-            <Card className={classes.cardroot}>
-            <CardActionArea>
+                </Card>
+                <Card className={classes.miniroot} elevation={3}>
+                <div className={classes.details}>
+                    <CardContent className={classes.content} >
+                    <Typography component="h5" variant="h5">
+                        Name Here
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                        job title
+                    </Typography>
+                    </CardContent>
+                </div>
                 <CardMedia
-                className={classes.media}
-                image={Job}
-                title="Contemplative Reptile"
+                    className={classes.cover}
+                    image={image}
+                    title="Live from space album cover"
                 />
-                <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    Title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    Short Description are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                    across all continents except Antarctica
-                </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-            <Button size="small" color="primary">
-                Status
-                </Button>
-            </CardActions>
-            </Card>
-
-            <Card className={classes.cardroot}>
-            <CardActionArea>
+                </Card>
+                <Card className={classes.miniroot} elevation={3}>
+                <div className={classes.details}>
+                    <CardContent className={classes.content} >
+                    <Typography component="h5" variant="h5">
+                        Name Here
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                        job title
+                    </Typography>
+                    </CardContent>
+                </div>
                 <CardMedia
-                className={classes.media}
-                image={Job}
-                title="Contemplative Reptile"
+                    className={classes.cover}
+                    image={image}
+                    title="Live from space album cover"
                 />
-                <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    Title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    Short Description are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                    across all continents except Antarctica
-                </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-            <Button size="small" color="primary">
-                Status
-                </Button>
-            </CardActions>
-            </Card>
-
-            <Card className={classes.cardroot}>
-            <CardActionArea>
-                <CardMedia
-                className={classes.media}
-                image={Job}
-                title="Contemplative Reptile"
-                />
-                <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    Title
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    Short Description are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                    across all continents except Antarctica
-                </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary">
-                Status
-                </Button>
-            </CardActions>
-            </Card>
+                </Card>
             </div>
             </div>
             </div>
@@ -277,5 +224,29 @@ const useStyles = makeStyles((theme) => ({
       },
       activity:{
         color: '#004b96'
-      }
+      },
+      details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+  miniroot: {
+    display: 'flex',
+    margin:30,
+  }
   }));
